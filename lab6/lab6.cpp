9bcecf6e6d96e722f0808049c90afd7a46fbb6f9
Anirudh-1149 <45738362+Anirudh-1149@users.noreply.github.com>
2019-09-04 21:52:14 +0530

    Create lab6.cpp

---
+++ lab6/lab6.cpp
@@ -0,0 +1,137 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+int maxi[2];
+struct Node{
+	int key;  
+	int amount;
+    Node *left;  
+    Node *right;  
+    int height;
+};
+
+int max(int a, int b)  
+{  
+    return (a > b)? a : b;  
+}  
+
+int height(Node *N)  
+{  
+    if (N == NULL)  
+        return 0;  
+    return N->height;  
+}
+
+Node* newNode(int key,int amount)  
+{  
+    Node* node = new Node(); 
+    node->key = key;  
+    node->amount = amount;
+    if(amount>maxi[0])
+    {
+        maxi[0] = amount;
+        maxi[1] = key;
+    }
+    node->left = NULL;  
+    node->right = NULL;  
+    node->height = 1;
+    return(node);  
+}  
+
+Node *rightRotate(Node *y)  
+{  
+    Node *x = y->left;  
+    Node *T2 = x->right;    
+    x->right = y;  
+    y->left = T2;  
+    y->height = max(height(y->left), 
+                    height(y->right)) + 1;  
+    x->height = max(height(x->left), 
+                    height(x->right)) + 1;  
+    return x;  
+}  
+
+Node *leftRotate(Node *x)  
+{  
+    Node *y = x->right;  
+    Node *T2 = y->left;    
+    y->left = x;  
+    x->right = T2;  
+    x->height = max(height(x->left),     
+                    height(x->right)) + 1;  
+    y->height = max(height(y->left),  
+                    height(y->right)) + 1;   
+    return y;  
+}  
+
+int getBalance(Node *N)  
+{  
+    if (N == NULL)  
+        return 0;  
+    return height(N->left) - height(N->right);  
+}  
+
+Node* insert(Node* node, int key,int amount)  
+{  
+    if (node == NULL)  
+        return(newNode(key,amount));  
+  
+    if (key < node->key)  
+        node->left = insert(node->left, key,amount);  
+    else if (key > node->key)  
+        node->right = insert(node->right, key,amount);  
+    else if(key == node->key){
+        node->amount+=amount;
+        if(node->amount > maxi[0]){
+            maxi[0] = node->amount;
+            maxi[1] = node->key;
+        }
+    }
+
+    node->height = 1 + max(height(node->left),  
+                        height(node->right));  
+  
+    int balance = getBalance(node);  
+  
+    if (balance > 1 && key < node->left->key)  
+        return rightRotate(node);  
+  
+    if (balance < -1 && key > node->right->key)  
+        return leftRotate(node);  
+    if (balance > 1 && key > node->left->key)  
+    {  
+        node->left = leftRotate(node->left);  
+        return rightRotate(node);  
+    }  
+    if (balance < -1 && key < node->right->key)  
+    {  
+        node->right = rightRotate(node->right);  
+        return leftRotate(node);  
+    }  
+    return node;  
+}  
+
+int main()
+{
+	Node*ptr = NULL;
+	int n;
+	cin>>n;
+    int T;
+    cin>>T;
+	while(T--){
+		int opp;
+		cin>>opp;
+		if(opp==1)
+		{
+			int key,amount;
+			cin>>key>>amount;
+			ptr = insert(ptr,key,amount);
+		}
+	
+    else
+        if(maxi[0] == 0)
+            cout<<"No data available"<<"\n";
+        else
+            cout<<maxi[1]<<"\n";
+    }
+	return 0;
+}
